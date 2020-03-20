3158628a700b3d0411f1150a2d99c3e1606465bc
Clinton Howell <chowell@rounding6.com>
2020-03-20 00:48:35 +0000

    quick demo script for posting to pushover

---
+++ pushovertest.py
@@ -0,0 +1,15 @@
+import http.client, urllib
+
+apitoken = "pushover api token"
+userkey = "pushover user key"
+
+conn = http.client.HTTPSConnection("api.pushover.net:443")
+conn.request("POST", "/1/messages.json",
+  urllib.parse.urlencode({
+    "token": apitoken,
+    "user": userkey,
+    "message": "hello world",
+  }), { "Content-type": "application/x-www-form-urlencoded" })
+conn.getresponse()
+
+
