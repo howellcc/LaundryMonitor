d62067a1312813d729d7d0f7e3b4c53797af2d0b
Clinton Howell <chowell@rounding6.com>
2020-03-26 20:48:21 -0400

    housekeeping on the keep running script

--- keepLaundryMonitorRunning.sh
+++ keepLaundryMonitorRunning.sh
@@ -1,14 +1,18 @@
 #! /bin/bash
 
+#this can be put into a crontab with the following
+# * * * * * /{insert path here}/keepLaundryMonitorRunning.sh >/dev/null 2>&1
+
 case "$(pidof python3 laundrymonitor.py | wc -w)" in
 
 0)  echo "Restarting Laundry Monitor:     $(date)" >> /home/chowell/laundry.txt
 	cd /home/chowell/LaundryMonitor
 	python3 laundrymonitor.py &
-    ;;
-1)  # all ok
-    ;;
-*)  echo "Removed double LaundryMonitor: $(date)" >> /home/chowell/laundry.txt
-    kill $(pidof python3 laundrymonitor.py | awk '{print $1}')
+	;;
+1)  # There is exactly 1 instance running. Nothing to do.
+	;;
+*)  # There is more than 1 instance running. Kill the last one.
+	echo "Removed double LaundryMonitor: $(date)" >> /home/chowell/laundry.txt
+	kill $(pidof python3 laundrymonitor.py | awk '{print $1}')
     ;;
 esac
