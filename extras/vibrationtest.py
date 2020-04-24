3c8a23e8e2e399b21fbe405fa101471c5ccd14dc
Clinton Howell <chowell@rounding6.com>
2020-04-24 01:48:22 +0000

    simplifying dryer testing

--- extras/vibrationtest.py
+++ extras/vibrationtest.py
@@ -4,29 +4,26 @@ import RPi.GPIO as GPIO
 #GLOBALS
 ISVIBRATING = False
 LAST_VIBRATION_TIME = datetime.datetime(9999,1,1,0,0,0)
-START_VIBRATION_TIME = datetime.MINYEAR
-SENSOR_PIN = 14
+VIBRATION_STOP_TIME_THRESHOLD_SEC = 30
+DRYER_SENSOR_PIN = 14
 
 def VibrationCallback(x):
     global ISVIBRATING
     global LAST_VIBRATION_TIME
-    global START_VIBRATION_TIME
     
     LAST_VIBRATION_TIME = datetime.datetime.now()
-    if not ISVIBRATING:
-        START_VIBRATION_TIME = LAST_VIBRATION_TIME
-        ISVIBRATING = True
+    ISVIBRATING = True
 
 def MonitorTheDryer():
     "This function monitors the dryer."
 
     global ISVIBRATING
     global LAST_VIBRATION_TIME
-    global START_VIBRATION_TIME
+    global VIBRATION_STOP_TIME_THRESHOLD_SEC
     
     current_time = datetime.datetime.now()
     timeDelta = current_time - LAST_VIBRATION_TIME
-    if(timeDelta.total_seconds() > 30): #has it stopped vibrating for 30 sec. 
+    if(timeDelta.total_seconds() > VIBRATION_STOP_TIME_THRESHOLD_SEC): #has it stopped vibrating for threshold. 
         ISVIBRATING = False
 
     if(ISVIBRATING):
@@ -38,9 +35,9 @@ def MonitorTheDryer():
 def Main():
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(SENSOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.add_event_detect(SENSOR_PIN, GPIO.RISING)
-    GPIO.add_event_callback(SENSOR_PIN, VibrationCallback)
+    GPIO.setup(DRYER_SENSOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.add_event_detect(DRYER_SENSOR_PIN, GPIO.RISING)
+    GPIO.add_event_callback(DRYER_SENSOR_PIN, VibrationCallback)
 
     #Time to get down to business
     while(1):
